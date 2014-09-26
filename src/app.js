7178198ef691f1dc561762d6f05beda5feb774ad
radiodario <dario.villanueva@gmail.com>
2014-09-26 17:01:45 +0100

    add a spinner

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ window.jQuery = $;
 window.$ = $;
 require('magnific-popup');
 
+var Spinner = require('spin.js');
+
 var db = new Firebase('https://visual-identity.firebaseio.com/');
 
 var gallery = document.querySelector('div.gallery');
@@ -14,8 +16,32 @@ var main = document.querySelector('.main');
 var popup = $('#detail-modal');
 
 
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 20, // The length of each line
+  width: 10, // The line thickness
+  radius: 30, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb or array of colors
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '50%' // Left position relative to parent
+};
+var target = document.querySelector('.spinhome');
+var spinner = new Spinner(opts).spin(target);
+
+
 db.child('images')
     .on('child_added', function(newImage) {
+
+        spinner.stop();
 
         var img = newImage.val();
 
