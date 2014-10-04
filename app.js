ffa33c287931aa4e7d2cf1634f5c61fc11e9004c
radiodario <dario.villanueva@gmail.com>
2014-10-04 11:17:23 +0100

    Updates

--- app.js
+++ app.js
@@ -11761,7 +11761,7 @@ var imagesRef = db.child('images');
 
 
 
-var gallery = document.querySelector('div.gallery');
+var gallery = document.querySelector('div.gallery .items');
 
 var main = document.querySelector('.main');
 
